ea468f713f5ae41e57b8fa9aa206ed75014dd9dc
Deborah Mchau <deborahenry94@yahoo.com>
2025-10-14 11:45:41 +0300

    fix: reinstall and configure Tailwind CSS properly

--- book-haven/src/App.jsx
+++ book-haven/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
       <div>
-        <h1 className="text-3xl font-bold text-center mt-8">Welcome to Book Haven!</h1>
+        <h1 className="text-3xl font-bold text-center bg-blue-500 mt-8">Welcome to Book Haven!</h1>
       </div>
   );
 }
