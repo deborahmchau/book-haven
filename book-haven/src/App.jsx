3aefc893d7e8cb41cb6dbd77729eceb434940f00
Deborah Mchau <deborahenry94@yahoo.com>
2025-10-14 12:13:01 +0300

    Install and set up routing in App.jsx

--- book-haven/src/App.jsx
+++ book-haven/src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
+import BookDetails from "./pages/BookDetails";
+import MyLibrary from "./pages/MyLibrary";
+import Recommendations from "./pages/Recommendations";
+
 function App() {
   return (
-      <div>
-        <h1 className="text-3xl font-bold text-center bg-blue-500 mt-8">Welcome to Book Haven!</h1>
+      <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/book/:id" element={<BookDetails />} />
+            <Route path="/library" element={<MyLibrary />} />
+            <Route path="/recommendations" element={<Recommendations />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
+    </Router>
   );
 }
 
